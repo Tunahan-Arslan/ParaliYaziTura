d281c5f7618410b069f364715e9ac9e491c56fd8
Tunahan Arslan <tuniaarslan27@gmail.com>
2023-06-01 13:19:57 +0300

    Proje dosyası ekle.

---
+++ ParaliYaziTura/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ParaliYaziTura
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtpara = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbakiye = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtpara
+            // 
+            this.txtpara.Location = new System.Drawing.Point(256, 348);
+            this.txtpara.Multiline = true;
+            this.txtpara.Name = "txtpara";
+            this.txtpara.Size = new System.Drawing.Size(141, 25);
+            this.txtpara.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 348);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Yatırılacak bakiyeyi gir:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 413);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mevcut bakiye:";
+            // 
+            // txtbakiye
+            // 
+            this.txtbakiye.Location = new System.Drawing.Point(256, 413);
+            this.txtbakiye.Multiline = true;
+            this.txtbakiye.Name = "txtbakiye";
+            this.txtbakiye.Size = new System.Drawing.Size(141, 25);
+            this.txtbakiye.TabIndex = 3;
+            this.txtbakiye.TextChanged += new System.EventHandler(this.txtbakiye_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(159, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Sayı Getir";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(117, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(224, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Rastgele Gelen Sayı = \' \'";
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(209, 35);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Bilgi için tıkla";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(159, 219);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 39);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Sayı Sıfırla";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtbakiye);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtpara);
+            this.Name = "Form1";
+            this.Text = "KUMAR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtpara;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbakiye;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ParaliYaziTura/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParaliYaziTura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int bakiye = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtbakiye.Text = bakiye.ToString();
+            MessageBox.Show("Hesabınıza 120 bakiye aktarıldı. Hedefiniz bakiyenizi 500 bakiyeye çıkartmak!");
+            bakiye = 120;
+            txtbakiye.Text = bakiye.ToString();
+
+        }
+
+        private void txtbakiye_TextChanged(object sender, EventArgs e)
+        {
+            txtbakiye.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("'Rastgele Sayı' butonuna tıklandığında 0-3(0 ve 3 dahil) arasında çıkan rakam, yatırılan bakiye ile çarpılır ve mevcut bakiyenize eklenir. Bakiyeniz 500 olduğunda oyunu kazanırsınız.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtpara.Text == "" || txtpara.Text == "0")
+            {
+                MessageBox.Show("Bakiye yatırma alanı boş veya 0 olamaz!");
+
+            }
+            else
+            {
+                Random random = new Random();
+                int rastgeleSayi = random.Next(0, 4);
+                label3.Text = "Rastgele Gelen Sayı = '" + rastgeleSayi.ToString() + "'";
+                int yatirilanPara = 0;
+                                yatirilanPara = Convert.ToInt32(txtpara.Text);
+                if (yatirilanPara > bakiye)
+                {
+                    MessageBox.Show("Girilen miktar hesabınızdaki paradan yüksek olduğu için yatırılamıyor!");
+                }
+                else
+                {
+                    if (rastgeleSayi == 0)
+                    {
+                        txtbakiye.Text = Convert.ToString(bakiye - yatirilanPara);
+                        bakiye -= yatirilanPara;
+                        txtpara.Text = "";
+                    }
+                    else if (rastgeleSayi == 1)
+                    {
+                        txtbakiye.Text = Convert.ToString(bakiye + yatirilanPara);
+                        bakiye += yatirilanPara;
+                        txtpara.Text = "";
+                    }
+                    else if (rastgeleSayi == 2)
+                    {
+                        txtbakiye.Text = Convert.ToString(bakiye + yatirilanPara * 2);
+                        bakiye += yatirilanPara * 2;
+                        txtpara.Text = "";
+                    }
+                    else if (rastgeleSayi == 3)
+                    {
+                        txtbakiye.Text = Convert.ToString(bakiye + yatirilanPara * 3);
+                        bakiye += yatirilanPara * 3;
+                        txtpara.Text = "";
+                    }
+                    if (bakiye >= 500)
+                    {
+                        MessageBox.Show("Tebrikler paranız şuanda =" + bakiye);
+                        DialogResult result = MessageBox.Show("Tekrar oynamak ister misin?", "Oyun Bitti", MessageBoxButtons.YesNo);
+                        if (result == DialogResult.Yes)
+                        {
+                            bakiye = 120;
+                            txtpara.Text = "";
+                            txtbakiye.Text = "120";
+                            label3.Text = "Rastele Gelen Sayı = ' '";
+                        }
+                        else if (result == DialogResult.No)
+                        {
+                            this.Close();
+                        }
+                    }
+                    else if (bakiye == 0)
+                    {
+                        MessageBox.Show("Maalesef paranız bitti. Oyun kapatılıyor!");
+                        this.Close();
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Rastgele Gelen Sayı = ' '";
+        }
+    }
+}
